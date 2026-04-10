f8dcdc11bbff74c5627499a4984b1ddb00423084
Talmaj Marinc <talmaj@comfy.org>
2026-04-10 15:03:05 +0200

    Remove sparkvsr related code.

--- nodes.py
+++ nodes.py
@@ -2459,8 +2459,7 @@ async def init_builtin_extra_nodes():
         "nodes_curve.py",
         "nodes_rtdetr.py",
         "nodes_frame_interpolation.py",
-        "nodes_sam3.py"
-        "nodes_cogvideox.py",
+        "nodes_sam3.py",
     ]
 
     import_failed = []
